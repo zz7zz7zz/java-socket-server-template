ada864036555c9cec519f2741a8ba004ccd08ab0
Long <longhui.yang@inveno.cn>
2017-12-21 20:25:47 +0800

    server模板工程 初始化!

---
+++ src/com/server/template/Main.java
@@ -0,0 +1,14 @@
+package com.server.template;
+/**
+ * author       :   long
+ * created on   :   2017/11/30
+ * description  :  服务器入口
+ */
+
+public class Main {
+
+    public static void main(String [] args){
+        MainNioServer.main_start(args); 
+    }
+
+}
